7aa89e85d1832312f14689a208ce292017cd216d
naveenkalyan0000 <127377317+naveenkalyan0000@users.noreply.github.com>
2024-01-11 12:42:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,6 +121,51 @@ resource "aws_route_table" "web-rt" {
     Name = "WebRT"
   }
 }
+resource "aws_nat_gateway" "nat" {
+  connectivity_type = "public"
+  allocation_id = "eipalloc-05d954cb42d6e9421"
+  subnet_id     = aws_subnet.web-subnet-1.id
+
+  tags = {
+    Name = "SWIGGY-nat"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
+
+
+
+
+# Create App layer route table
+resource "aws_route_table" "application-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "NatRT"
+  }
+}
+
+
+# Create App Subnet association with App route table
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.application-subnet-1.id
+  route_table_id = aws_route_table.application-rt.id
+}
+
+resource "aws_route_table_association" "d" {
+  subnet_id      = aws_subnet.application-subnet-2.id
+  route_table_id = aws_route_table.application-rt.id
+}
 
 # Create Web Subnet association with Web route table
 resource "aws_route_table_association" "a" {
